38c3851943a2e6e0ef832e97b8ffa06a8cf5154d
ashtrobff <ashishp1729@gmail.com>
2023-01-27 12:17:47 +0530

    this is a test

---
+++ website/control.js
@@ -0,0 +1,7 @@
+var btn= document.getElementById("toggle");
+var light= document.getElementById("throw");
+
+function lightup(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
